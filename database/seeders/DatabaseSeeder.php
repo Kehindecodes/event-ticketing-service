4f5d4237dfe95fcb4d9149e6267d077f44f64a9b
Kehinde Oki <olamiposi.oki@gmail.com>
2026-09-05 12:34:08 +0100

    update user model and seed sample data to users tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // User::factory(3)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'id' => \Ramsey\Uuid\Uuid::uuid4(),
+            'name' => 'Test User 2',
+            'email' => 'oladotundev@gamil.com',
+
         ]);
     }
 }
